1e2e972455f8256454a9e0c681d93f32ac35c9e2
pierre <pnavarin@gmail.com>
2022-06-05 21:15:55 -0400

    Refactor

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,15 +1,34 @@
 class Api::MessagesController < ApplicationController
-    def index
-        if params[:channel_id]
-            channel = Channel.find(params[:channel_id])
-            @messages= Message.all.where(
-                messageable_type: 'Channel',
-                messageable_id: channel.id
-            )
-        end
-        render :index
+    
+    def index 
+        @messages = Message.get_by_channel(channel_id) 
     end
+
+    def update
+        @message = Message.find(params[:id])
+        if @message.update
+            render :show
+        else 
+            @message.errors.full_messages
+        end  
+
+    end 
 
     
 
+
+    def destroy 
+        @message = Message.find(params[:id])
+        @message.destroy!
+    end 
+
+    def message_params
+        params.require(:message).permit(:body, :author_id, :channel_id)
+    end 
+
+    def params 
+        params[:parent_message_id]
+    end 
+
+   
 end
